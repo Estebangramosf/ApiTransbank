a5470cb13214ac6a11d124e0e4ffe55489402c6d
Esteban Ramos Fernandez <eramos@celmedia.cl>
2016-12-02 18:23:44 -0300

    update - validate that the user does the transanction in case of no has points to buy, in case that the user has the poins, the transaction consuming his points

--- app/Http/Controllers/CelmediaPagoController.php
+++ app/Http/Controllers/CelmediaPagoController.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Parser;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Redirect;
 use SimpleXMLElement;
 use Artisaninweb\SoapWrapper\Facades\SoapWrapper;
 use App\User;
@@ -107,38 +108,66 @@ class CelmediaPagoController extends Controller
           return view('webpay.celmediaPago',['request'=>$request->all()]);
         }
 
+        //Se crea el objeto $userResult como resultado de la verificación del usuario
+        $userResult = $this->verifyRUTExistanceAndGetUser($request);
 
-        if($request->TBK_RUT && isset($request->TBK_RUT)){
+        $request->TBK_MONTO=str_replace(".","",$request->TBK_MONTO);
+        if( $userResult->pts > $request->TBK_MONTO ){
+          //En esta parte se debiese hacer el canje normal sin problemas
+          echo "Los puntos le alcanzan . <br>".
+            'Pts Usuario : '.($userResult->pts .' | Costo : '. $request->TBK_MONTO).'<br>'.
+            'Total restante después del canje : '.($userResult->pts - $request->TBK_MONTO);
 
-          //Obtain data from client
-          $this->ClientData = $this->ConsultaPuntosWSCLOTPC($request->TBK_RUT);
+        }else{
+          //En esta parte se debiese guardar los datos y generar el pago por transbank para el usuario
+          //Tomando la diferencia de los puntos y generar el cobro en base a los puntos
 
-
-
-
-          /*
-          if($request->TBK_MONTO && $request->TBK_MONTO ){
-            dd(1);
-          }
-          */
-
-          //Return data at view to send form
-
+          echo "Los puntos no le alcanzan . <br>".
+            'Pts Usuario : '.($userResult->pts .' | Costo : '. $request->TBK_MONTO).'<br>'.
+            'Total restante después del canje : '.($userResult->pts - $request->TBK_MONTO).'<br>'.
+            'Se debe generar el pago mediante transbank por los puntos restantes.';
         }
+
+
       }catch(Exception $e){
 
       }
+
+
+
     }
 
+    //Funcion que verifica si el rut del usuario existe mediante request y devuelve al usuario como objeto de la clase
+    //En caso que el usuario no exista, redirecciona al usuario
+    public function verifyRUTExistanceAndGetUser(Request $request){
+      try{
+        if($request->TBK_RUT && isset($request->TBK_RUT)){
+          //Obtiene la informacion del cliente, si no existe lo registra, si existe, actualiza los puntos desde WS
+          $this->ConsultaPuntosWSCLOTPC($request->TBK_RUT);
+          //Se busca al usuario en la base de datos local
+          $user = User::where('rut', (int)$request->TBK_RUT)->get();
+          //se verifica si existe y lo guarda en una veriable
+          if(isset($user[0])){
+            return json_decode(json_encode($user[0]));
+          }else{
+            return Redirect::to('http://ecorpbancadesa.celmedia.cl/');
+          }
+        }
+      }catch(Exception $e){}
+    }
+
+    //Funcion que verifica los puntos de un cliente y registra al usuario en caso que no exista en la base CelmediaPago
     public function ConsultaPuntosWSCLOTPC($rut){
+      //Se asegura en caso de caidas
       try {
+        //Se instancia un nuevo comunicador de webservice con SoapWrapper
         SoapWrapper::add(function ($service) {
           $service
             ->name('currency')
             ->wsdl('http://190.196.23.184/clop_otpc_web_prestashop/wscl/wsclotpc_server_ps.php?wsdl')
             ->trace(true);
         });
-
+        //Se definen los parametros que consume el webservice
         $data = [
           'rut'         => $rut,
           'usuario'     => 'tienda_ps',
@@ -148,21 +177,25 @@ class CelmediaPagoController extends Controller
         ];
 
 
-        // Using the added service
+        // Se usa el nuevo webservice creado
         SoapWrapper::service('currency', function ($service) use ($data) {
           $ClientData = $service->call('ConsultaPuntosWSCLOTPC', [$data]);
-          
+
+          //Se busca al usuario en la base de datos local
           $user = User::where('rut', (int)$ClientData->rut)->get();
+          //se verifica si existe y lo guarda en una veriable
           if(isset($user[0])){
             $user = json_decode(json_encode($user[0]));
           }
+          //Se verifica si el tiene rut válido
           if(isset($user->rut)){
-
+            //Se busca y actualizan los puntos del usuario, luego se guarda
             $user=User::findOrFail($user->id);
-            $user->pts = $ClientData->SaldoPuntos;
+            $user->pts = $ClientData->SaldoPuntos;//30000;//
             $user->save();
             return true;
           }else{
+            //En caso que el usuario no exista en base se registra y se guarda
             User::create([
               'name'=>$ClientData->Nombre,
               'rut'=>$ClientData->rut,
